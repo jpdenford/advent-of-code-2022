7163443bfe1f9c0f26eec50d3cdc16fdba9b7ce5
JP Denford <jpdenford@gmail.com>
2022-12-06 21:54:05 +1300

    add day1 part 2

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -7,16 +7,21 @@ pub fn run() {
     let file_path = "src/day1/input.txt";
     let contents = fs::read_to_string(file_path).expect("Couldn't open file!");
 
-    let max_calorie: i32 = contents
+    let mut elf_calories_dec: Vec<i32> = contents
         .split("\n\n")
-        .map(|reindeer_block| {
-            reindeer_block
+        .map(|elf_block| {
+            elf_block
                 .split("\n")
                 .map(|numstr| numstr.parse::<i32>().unwrap())
                 .sum::<i32>()
         })
-        .max()
-        .unwrap();
+        .collect::<Vec<i32>>();
 
-    println!("day1 answer: {}", max_calorie);
+    elf_calories_dec.sort_by(|n1, n2| n2.partial_cmp(n1).unwrap());
+
+    let max_calories = elf_calories_dec.first().unwrap();
+    println!("day1 part1 answer: {}", max_calories);
+
+    let sum_top_three: i32 = elf_calories_dec.iter().take(3).sum();
+    println!("day1 part2 answer: {}", sum_top_three);
 }
